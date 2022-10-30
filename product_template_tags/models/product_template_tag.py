692df75729a86b741b44ccb63a3937469cfa0b49
Nhomar Hernández [Vauxoo] <nhomar@vauxoo.com>
2022-10-29 23:42:56 -0500

    [IMP] product_template_tags: Now it is possible to group the tags. (#22)

--- product_template_tags/models/product_template_tag.py
+++ product_template_tags/models/product_template_tag.py
@@ -2,14 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplateTag(models.Model):
     _name = "product.template.tag"
     _description = "Product Tag"
     _order = "sequence, name"
+    _parent_store = True
 
     name = fields.Char(required=True, translate=True)
+    active = fields.Boolean(default=True)
     sequence = fields.Integer(default=10)
     color = fields.Integer(string="Color Index")
     product_tmpl_ids = fields.Many2many(
@@ -27,6 +30,12 @@ class ProductTemplateTag(models.Model):
         string="Company",
         default=lambda self: self.env.company,
     )
+    child_ids = fields.One2many('res.partner.category', 'parent_id', string='Child Tags')
+    parent_id = fields.Many2one(
+        "product.template.tag"
+    )
+    parent_path = fields.Char(index=True)
+    partner_ids = fields.Many2many('res.partner', column1='category_id', column2='partner_id', string='Partners')
 
     _sql_constraints = [
         (
@@ -50,3 +59,38 @@ class ProductTemplateTag(models.Model):
             tag_id_product_count = dict(self.env.cr.fetchall())
         for rec in self:
             rec.products_count = tag_id_product_count.get(rec.id, 0)
+
+    def name_get(self):
+        """ Return the tag' display name, including their direct
+            parent by default.
+
+            If ``context['product_template_tag']`` is ``'short'``, the short
+            version of the tag name (without the direct parent) is used.
+            The default is the long version.
+        """
+        if self._context.get('product_template_tag') == 'short':
+            return super(ProductTemplateTag, self).name_get()
+
+        res = []
+        for tag in self:
+            names = []
+            current = tag
+            while current:
+                names.append(current.name)
+                current = current.parent_id
+            res.append((tag.id, ' / '.join(reversed(names))))
+        return res
+
+    @api.constrains('parent_id')
+    def _check_parent_id(self):
+        if not self._check_recursion():
+            raise ValidationError(_('You can not create recursive tags.'))
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        if name:
+            # Be sure name_search is symetric to name_get
+            name = name.split(' / ')[-1]
+            args = [('name', operator, name)] + args
+        return self._search(args, limit=limit, access_rights_uid=name_get_uid)
